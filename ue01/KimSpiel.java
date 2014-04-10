3028399f4bc8fc2fbb6b82dd18300c8e65170b19
sighter <sighter@resource-dnb.de>
2014-04-11 00:59:50 +0200

    added KimSpiel class stub, KimSpielDialog and gitignore

---
+++ ue01/KimSpiel.java
@@ -0,0 +1,17 @@
+public class KimSpiel {
+
+    private int digits;
+    private int lastNumber;
+
+    public KimSpiel() {
+        this.digits = 2;
+    }
+
+    public int getDigits() {
+        return this.digits;
+    }
+
+    public int getNextNumber() {
+        return 23;
+    }
+}
--- ue01/KimSpielDialog.java
+++ ue01/KimSpielDialog.java
@@ -1,11 +1,14 @@
 
 import Tools.IO.*;
-
+import java.io.*;
 
 public class KimSpielDialog {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Testing run once");
+
+        KimSpielDialog dialog = new KimSpielDialog();
+        dialog.runOnce();
     }
 
     private KimSpiel game;
@@ -18,8 +21,28 @@ public class KimSpielDialog {
 
     }
 
-    public boolean runOnce(KimSpiel game) {
+    public boolean runOnce() {
+
         System.out.println("Sie haben ein neues Kimspiel gestartet.");
+        
+        this.game = new KimSpiel();
+
+        int number = this.game.getNextNumber();
+        int digits = this.game.getDigits();
+
+        System.out.println("Merken sie sich folgende " + digits +"-stellige Nummer: " + number);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        for (int i = 0; i < 200; ++i) System.out.println();
+        System.out.println("Bitte geben sie jetzt die Zahl ein");
+        
+        
+
+        return true;
     }
 
     public void loopPlay() {
@@ -31,6 +54,8 @@ public class KimSpielDialog {
     }
     
     private boolean readNumber() {
+
+        return true;
 
     }
 }
