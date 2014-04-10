e6239c4ceb4b2b904ec645cb3d5774053f413d29
sighter laptop <sighter@resource-dnb.de>
2014-04-10 23:50:47 +0200

    initial commit

---
+++ ue01/KimSpielDialog.java
@@ -0,0 +1,36 @@
+
+import Tools.IO.*;
+
+
+public class KimSpielDialog {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+
+    private KimSpiel game;
+
+    /**
+     * constructor
+     * @return [description]
+     */
+    public KimSpielDialog() {
+
+    }
+
+    public boolean runOnce(KimSpiel game) {
+        System.out.println("Sie haben ein neues Kimspiel gestartet.");
+    }
+
+    public void loopPlay() {
+
+    }
+    
+    private void showNumber(int number) {
+
+    }
+    
+    private boolean readNumber() {
+
+    }
+}
