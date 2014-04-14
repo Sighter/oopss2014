f3b7cab1a954b88e854ae32cea5115f03d68deee
sighter laptop <sighter@resource-dnb.de>
2014-04-14 14:00:17 +0200

    implemented stub method for getting changing numbers

--- ue01/KimSpiel.java
+++ ue01/KimSpiel.java
@@ -3,22 +3,31 @@ import java.util.Arrays;
 public class KimSpiel {
 
     private int digits;
+    private int maxDigits;
     private int[] lastNumber;
 
     public KimSpiel() {
         this.digits = 2;
+        this.maxDigits = 5;
     }
 
     public int getDigits() {
         return this.digits;
     }
 
+    public int getMaxDigits() {
+        return this.maxDigits;
+    }
+
     public int[] getNextNumber() {
-        int[] numberArray = {
-            1, 4, 5
-        };
+        int[] numberArray = new int[this.getDigits()];
+
+        for (int idx = 0; idx < this.getDigits(); idx++) {
+            numberArray[idx] = 2;
+        }
 
         this.lastNumber = numberArray;
+        this.digits += 1;
 
         return numberArray;
     }
