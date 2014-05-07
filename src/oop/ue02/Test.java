1c91b7dd0705fcf4cf01e53dec892af8d61b0f5c
sighter <sighter@resource-dnb.de>
2014-05-07 17:35:36 +0200

    added test runner class

---
+++ src/oop/ue02/Test.java
@@ -0,0 +1,11 @@
+import org.junit.runner.*;
+import org.junit.runner.notification.Failure;
+
+public class Test {
+
+    public static void main(String[] args) {
+        JUnitCore.main("oop.ue02.tests.TimeAbleTest");
+
+    }
+
+}
